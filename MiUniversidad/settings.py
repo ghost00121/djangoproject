c7fc871b8237b4ae41fa8791c9e4e8b8f33a6158
Marco Cueva <marco2000marck@gmail.com>
2023-06-14 18:42:20 -0500

    Commented pip install

--- MiUniversidad/settings.py
+++ MiUniversidad/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,14 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7b2$mrd2)ulx-^uwp1y2vct14lh$gf^u(=(*u2gl=wsbi_i%#3'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
-
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 # Application definition
 
 INSTALLED_APPS = [
@@ -48,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'MiUniversidad.urls'
@@ -75,19 +80,25 @@ WSGI_APPLICATION = 'MiUniversidad.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': "django1",
-        'USER': "root",
-        'PASSWORD': '',
-        'HOST': "127.0.0.1",
-        'PORT': "3306",
-        "OPTIONS": {
-            "sql_mode": "STRICT_ALL_TABLES",}
+        'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
+}
+
+
+    #    'ENGINE': 'django.db.backends.mysql',
+     #   'NAME': "django1",
+      #  'USER': "root",
+       ##'HOST': "127.0.0.1",
+        #'PORT': "3306",
+        #"OPTIONS": {
+         #   "sql_mode": "STRICT_ALL_TABLES",}
         
 
-    }
-}
+    
+
 
 
 # Password validation
@@ -126,6 +137,14 @@ USE_TZ = False
 
 STATIC_URL = 'static/'
 
+if not DEBUG:
+    # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory on Render.
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+    # Turn on WhiteNoise storage backend that takes care of compressing static files
+    # and creating unique names for each version so they can safely be cached forever.
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -o errexit
 
 #poetry install
 
-pip install -r requerements.tt
+#pip install -r requerements.tt
 python manage.py collectstatic --no-input
 python manage.py migrate
