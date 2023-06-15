65cb040f023b2eec4f1eff735160807f98249dc4
Marco Cueva <marco2000marck@gmail.com>
2023-06-14 21:45:23 -0500

    build.sh test4

--- MiUniversidad/settings.py
+++ MiUniversidad/settings.py
@@ -80,11 +80,8 @@ WSGI_APPLICATION = 'MiUniversidad.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-        'default': dj_database_url.config(
-        # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-    )
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    
 }
 
 
